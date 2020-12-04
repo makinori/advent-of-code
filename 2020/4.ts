7103d9c4bb2f94a8aaa2d9fd0986e5f0bb7d2a46
Maki <mxmcube@gmail.com>
2020-12-04 23:06:50 +0000

    Finished day 4

---
+++ 2020/4.ts
@@ -0,0 +1,99 @@
+import fs from "fs";
+
+const passports = fs
+	.readFileSync(__dirname + "/4.txt", "utf8")
+	.split(/\n\s*\n/g)
+	.map(passport =>
+		passport
+			.replace(/\s/g, "\n")
+			.split("\n")
+			.map(field => field.trim())
+	)
+	.map(fields => {
+		const data = {};
+		for (const field of fields) {
+			const [key, value] = field.split(":");
+			if (key && value) {
+				data[key] = value;
+			}
+		}
+		return data;
+	});
+
+{
+	const required = [
+		"byr", // Birth Year
+		"iyr", // Issue Year
+		"eyr", // Expiration Year
+		"hgt", // Height)
+		"hcl", // Hair Color
+		"ecl", // Eye Color
+		"pid" // Passport ID
+		// "cid", // Country ID
+	];
+
+	let validPassports = 0;
+
+	for (const passport of passports) {
+		let valid = true;
+		for (const field of required) {
+			if (passport[field] == null) {
+				valid = false;
+				break;
+			}
+		}
+		if (valid) validPassports++;
+	}
+
+	console.log(validPassports);
+}
+
+const checkYear = (yearStr: string, min: number, max: number) => {
+	if (yearStr == null) return false;
+	if (yearStr.length != 4) return false;
+	const year = Number(yearStr);
+	if (year < min) return false;
+	if (year > max) return false;
+	return true;
+};
+
+const checkHeight = (heightStr: string) => {
+	if (heightStr == null) return false;
+	const ext = heightStr.slice(-2).trim();
+	if (ext != "cm" && ext != "in") return false;
+	const height = Number(heightStr.slice(0, -2));
+	if (ext == "cm") {
+		if (height < 150) return false;
+		if (height > 193) return false;
+	} else if (ext == "in") {
+		if (height < 59) return false;
+		if (height > 76) return false;
+	}
+	return true;
+};
+
+const checkHexColor = (hexStr: string) =>
+	/^#[0-9a-f]{6}$/.test((hexStr || "").toLowerCase());
+
+const checkEyeColor = (eyeColor: string) =>
+	["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(eyeColor);
+
+const checkPassportId = (passportId: string) =>
+	/^[0-9]{9}$/.test(passportId || "");
+
+// const checkPass
+
+let validPassports = 0;
+
+for (const passport of passports) {
+	if (!checkYear(passport["byr"], 1920, 2002)) continue;
+	if (!checkYear(passport["iyr"], 2010, 2020)) continue;
+	if (!checkYear(passport["eyr"], 2020, 2030)) continue;
+	if (!checkHeight(passport["hgt"])) continue;
+	if (!checkHexColor(passport["hcl"])) continue;
+	if (!checkEyeColor(passport["ecl"])) continue;
+	if (!checkPassportId(passport["pid"])) continue;
+	validPassports++;
+}
+
+console.log(validPassports);
