97f501f90488598550ec133bbb9d19ea72da9172
Maki <mxmcube@gmail.com>
2020-12-06 20:00:44 +0000

    Solved the traveling merchant problem!

---
+++ 2015/9.ts
@@ -0,0 +1,75 @@
+import fs from "fs";
+
+const allRoutes = fs
+	.readFileSync(__dirname + "/9.txt", "utf8")
+	.split("\n")
+	.map(route => {
+		const matches = route.match(/^([^]+) to ([^]+) = ([0-9]+)$/);
+		return {
+			from: matches[1],
+			to: matches[2],
+			distance: Number(matches[3])
+		};
+	});
+
+const getDistance = (a: string, b: string) => {
+	for (const route of allRoutes) {
+		if (
+			(route.from == a && route.to == b) ||
+			(route.from == b && route.to == a)
+		) {
+			return route.distance;
+		}
+	}
+	return null;
+};
+
+const getAllNames = () => {
+	const names = {};
+	for (const route of allRoutes) {
+		names[route.from] = true;
+		names[route.to] = true;
+	}
+	return Object.keys(names);
+};
+
+const allNames = getAllNames();
+
+const filteredArray = (array: string[], filter: string[]) => {
+	const filtered = [];
+	for (const item of array) {
+		if (!filter.includes(item)) filtered.push(item);
+	}
+	return filtered;
+};
+
+const findAllCombinations = (allItems: string[]) => {
+	const combinations: string[][] = [];
+	const iterate = (items: string[], combination: string[] = []) => {
+		for (const item of items) {
+			const newCombination = [...combination, item];
+			if (newCombination.length == allItems.length) {
+				combinations.push(newCombination);
+			} else {
+				iterate(filteredArray(items, newCombination), newCombination);
+			}
+		}
+	};
+	iterate(allItems);
+	return combinations;
+};
+
+const allPaths = findAllCombinations(allNames)
+	.map(path => {
+		let distance = 0;
+		let currentlyAt = path[0];
+		for (let i = 1; i < path.length; i++) {
+			distance += getDistance(currentlyAt, path[i]);
+			currentlyAt = path[i];
+		}
+		return { path, distance };
+	})
+	.sort((a, b) => a.distance - b.distance);
+
+console.log(allPaths[0]);
+console.log(allPaths[allPaths.length - 1]);
