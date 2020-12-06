3b05c10f5a786ad38ba6912a60859246aff8a3dd
Maki <mxmcube@gmail.com>
2020-12-06 15:24:35 +0000

    Finished day 6

---
+++ 2020/6.ts
@@ -0,0 +1,56 @@
+import fs from "fs";
+
+const groups = fs
+	.readFileSync(__dirname + "/6.txt", "utf8")
+	.split(/\n\n/g)
+	.map(group => group.split(/\n/g));
+
+const makeLetterMap = (a: string) => {
+	const map = {};
+	for (const char of a.split("")) {
+		if (/[a-z]/.test(char)) map[char] = true;
+	}
+	return map;
+};
+
+console.log(
+	groups
+		.map(group => {
+			let letterMap = {};
+			for (const person of group) {
+				letterMap = { ...letterMap, ...makeLetterMap(person) };
+			}
+			return Object.keys(letterMap).length;
+		})
+		.reduce((a, b) => a + b)
+);
+
+const andArray = (a: string[], b: string[]) => {
+	const common: string[] = [];
+	for (const char of [...a, ...b]) {
+		if (a.includes(char) && b.includes(char) && !common.includes(char)) {
+			common.push(char);
+		}
+	}
+	return common;
+};
+
+console.log(
+	groups
+		.map(group => {
+			let initial = false;
+			let letters: string[] = [];
+			for (const person of group) {
+				const personLetters = Object.keys(makeLetterMap(person));
+				if (initial == false) {
+					initial = true;
+					letters = personLetters;
+				} else {
+					// and
+					letters = andArray(letters, personLetters);
+				}
+			}
+			return letters.length;
+		})
+		.reduce((a, b) => a + b)
+);
