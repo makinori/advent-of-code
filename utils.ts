171e1900fc45e70214c78d5ac662f1128fcf8371
Maki <mxmcube@gmail.com>
2020-12-12 05:03:40 +0000

    Updated relativeResolve to support reading from urls

--- utils.ts
+++ utils.ts
@@ -1,8 +1,8 @@
+const pathDelimiter = Deno.build.os == "windows" ? "\\" : "/";
+
 export const relativeResolve = (importMeta: ImportMeta, path: string) =>
-	importMeta.url
-		.replace(/^file:\/\//, "")
-		.split("/")
-		.slice(0, -1)
-		.join("/") +
-	"/" +
-	path;
+	new URL(
+		importMeta.url.split(pathDelimiter).slice(0, -1).join(pathDelimiter) +
+			pathDelimiter +
+			path
+	);
